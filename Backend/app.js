79bae8b79ce78372afdd25c01b948b14b262a5ea
Jatin Yadav <heroraoshab2005@gmail.com>
2025-11-19 00:17:25 +0530

    Add login, logout, and user profile functionality

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const connectToDB = require('./db/db')
 const userRoutes = require('./routes/user.routes')
 
@@ -11,6 +12,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
 app.get('/',(req,res)=>{
     res.send("Hello World!")
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,11 +2,21 @@ const express = require('express')
 const router = express.Router()
 const {body} = require('express-validator')
 const userController = require('../controllers/user.controller')
+const authMiddleware = require('../middlewares/auth.middleware')
 
 router.post('/register',[
     body('email').isEmail().withMessage("Invalid Email"),
     body('fullName.firstName').isLength({min:3}).withMessage("First name must be atleast 3 character long"),
     body("password").isLength({min:6}).withMessage("Password length must be at least 6 character long")
 ],userController.registerUser)
+
+router.post('/login',[
+    body('email').isEmail().withMessage("Invalid email"),
+    body("password").isLength({min:6}).withMessage("Password must be at least 6 characters")
+],userController.loginUser)
+
+router.get('/profile',authMiddleware.authUser,userController.getUserProfile)
+
+router.get('/logout',authMiddleware.authUser,userController.logoutUser)
 
 module.exports = router
