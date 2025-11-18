5f8b24570026692713eb95cd235d72efef04776e
Jatin Yadav <heroraoshab2005@gmail.com>
2025-11-18 22:58:58 +0530

    Add user registration functionality

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,21 @@
 require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
+const connectToDB = require('./db/db')
+const userRoutes = require('./routes/user.routes')
+
+connectToDB()
 
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
     res.send("Hello World!")
 })
+
+app.use('/users',userRoutes)
 
 module.exports = app;
