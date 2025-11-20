be491d194d5ceec14e5b831a589c399c7c76d21d
Jatin Yadav <heroraoshab2005@gmail.com>
2025-11-20 22:55:27 +0530

    Add captain login, logout and profile functionality

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
-const blackListTokenModel = require('../models/blackListToken.model')
+const blackListTokenModel = require("../models/blackListToken.model");
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
@@ -11,10 +11,10 @@ module.exports.registerUser = async (req, res, next) => {
 
   const { fullName, email, password } = req.body;
 
-  const isUserAlreadyExist = userModel.findOne({email});
-      if(isUserAlreadyExist){
-          return res.status(400).json({message: "user already exist"})
-      }
+  const isUserAlreadyExist = userModel.findOne({ email });
+  if (isUserAlreadyExist) {
+    return res.status(400).json({ message: "user already exist" });
+  }
 
   const hashPassword = await userModel.hashPassword(password);
 
@@ -36,40 +36,39 @@ module.exports.loginUser = async (req, res, next) => {
     return res.status(400).json({ erros: errors.array() });
   }
 
-  const {email,password} = req.body;
+  const { email, password } = req.body;
 
-  const user = await userModel.findOne({email}).select('+password')
+  const user = await userModel.findOne({ email }).select("+password");
 
-  if(!user){
-    return res.status(401).json({message:"Invalid email or password"})
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
   }
 
-  const isMatch = await user.comparePassword(password)
+  const isMatch = await user.comparePassword(password);
 
-  if(!isMatch){
-    return res.status(401).json({message:"Invalid email or password"})
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
   }
 
-  const token = user.generateAuthToken()
+  const token = user.generateAuthToken();
 
-  res.cookie('token',token)
+  res.cookie("token", token);
 
   res.status(201).json({
-   user,
-   token
-  })
-
+    user,
+    token,
+  });
 };
 
-module.exports.getUserProfile = async(req,res,next)=>{
-    res.status(200).json(req.user)
-}
+module.exports.getUserProfile = async (req, res, next) => {
+  res.status(200).json(req.user);
+};
 
-module.exports.logoutUser = async(req,res,next)=>{
-    res.clearCookie('token')
-    const token = req.cookies.token || req.headers.authorization.split(' '[1])
-    
-    await blackListTokenModel.create({token})
+module.exports.logoutUser = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
 
-    res.status(200).json({message:"logged out"})
-}
+  await blackListTokenModel.create({ token });
+  res.clearCookie("token");
+
+  res.status(200).json({ message: "logged out" });
+};
--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {body} =require('express-validator')
 const router = express.Router()
 const captainController = require('../controllers/captain.controller')
+const authMiddleware = require('../middlewares/auth.middleware')
 
 router.post('/register',[
     body('email').isEmail().withMessage('Invalid email'),
@@ -15,5 +16,16 @@ router.post('/register',[
 ],
 captainController.registerCaptain
 )
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({min:6}).withMessage('password must be atleast 6 character long')
+],
+captainController.loginCaptain
+)
+
+router.get('/profile',authMiddleware.authCaptain,captainController.getCaptainProfile)
+
+router.get('/logout',authMiddleware.authCaptain, captainController.logoutCaptain)
 
 module.exports = router
