fc884393a6756fd3e68473149415cca5f42457aa
Jatin Yadav <heroraoshab2005@gmail.com>
2025-11-29 20:59:28 +0530

    feat: add captain home page

---
+++ frontend/src/components/CaptainDetails.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const CaptainDetails = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-8">
+        <div className="flex items-center justify-start gap-4">
+          <img
+            className="w-16 rounded-full h-16 object-cover"
+            src="https://imgs.search.brave.com/HcOsG-F8M33AfmSy-SRJUOI0agp_YCqYqQXQ53AEOI4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMjE3/NTMxNDE0Ni9waG90/by95b3VuZy13b21h/bi10YWtpbmctYS1z/ZWxmaWUtaW4tbWV0/cm9wb2xpdGFuLXBh/cmstd2l0aC1hLXZp/ZXctdG8tZmluYW5j/aWFsLWRpc3RyaWN0/LW4tc2FudGlhZ28u/d2VicD9hPTEmYj0x/JnM9NjEyeDYxMiZ3/PTAmaz0yMCZjPS1Z/cFBuSkVYRnR1YmFG/QVVVcVF5NEV3TTZ3/NU9qc3J5RjlqNnd4/VnNvMk09"
+            alt=""
+          />
+          <h4 className="text-lg font-medium">Raosahabni</h4>
+        </div>
+        <div>
+          <h5 className="text-xl font-semibold">₹205.12</h5>
+          <p className="text-sm text-gray-600">Earned</p>
+        </div>
+      </div>
+      <div className="p-3 bg-gray-100 rounded-xl flex justify-evenly items-start gap-5 h-full">
+        <div className="text-center">
+          <i className="text-3xl mb-2 font-thin ri-time-line"></i>
+          <h5 className="text-lg font-medium mb-1">10.2</h5>
+          <p className="text-sm text-gray-600">Hours Online</p>
+        </div>
+        <div className="text-center">
+          <i className="text-3xl mb-2 font-thin ri-speed-up-line"></i>
+          <h5 className="text-lg font-medium mb-1">10.2</h5>
+          <p className="text-sm text-gray-600">Hours Online</p>
+        </div>
+        <div className="text-center">
+          <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
+          <h5 className="text-lg font-medium mb-1">10.2</h5>
+          <p className="text-sm text-gray-600">Hours Online</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainDetails;
--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import CaptainDetails from "../components/CaptainDetails";
 
 const CaptainHome = () => {
   return (
-    <div>CaptainHome</div>
-  )
-}
+    <div className="h-screen w-screen">
+      <div className="fixed p-6 top-0 flex justify-between w-full items-center">
+        <img
+          className="w-20"
+          src="https://ik.imagekit.io/raosahab/image-removebg-preview.png"
+          alt=""
+        />
+        <Link
+          to="/home"
+          className="h-10 w-10 bg-white flex items-center justify-center rounded-full cursor-pointer"
+        >
+          <i className="text-lg font-medium ri-logout-box-r-line"></i>
+        </Link>
+      </div>
+      <div className="h-3/5 w-screen">
+        <img
+          src="https://imgs.search.brave.com/pToz-Wc75oi6k8BWvFknE6YjADNc34pUQQYZlkQ7qqo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9pbWFnZS1tYXAt/c3RyZWV0cy13aXRo/LXRydWNrc18yMDc2/MzQtMjE4MC5qcGc_/c2VtdD1haXNfaHli/cmlk"
+          className="h-full w-full object-cover"
+          alt=""
+        />
+      </div>
+      <div className="h-2/5 p-6 flex flex-col justify-between">
+        <CaptainDetails />
+      </div>
+      <div className="fixed w-full z-10 bottom-0 px-3 py-6 pt-12 bg-white">
+          
+        </div>
+    </div>
+  );
+};
 
-export default CaptainHome
+export default CaptainHome;
