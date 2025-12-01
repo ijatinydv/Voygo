e071baa7483441e573c00b17d8f282f367c6be3d
Jatin Yadav <heroraoshab2005@gmail.com>
2025-12-01 13:06:52 +0530

    feat(api): add ride creation with otp

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,8 @@ const cookieParser = require("cookie-parser");
 const connectToDB = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
-const mapsRoutes = require("./routes/maps.routes")
+const mapsRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/ride.routes");
 
 connectToDB();
 
@@ -16,12 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.use("/users", userRoutes);
 app.use("/captains", captainRoutes);
-app.use('/maps',mapsRoutes)
+app.use("/maps", mapsRoutes);
+app.use("/rides", rideRoutes);
 
 module.exports = app;
