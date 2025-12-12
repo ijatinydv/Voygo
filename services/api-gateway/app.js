84897630187b9a6f13fe4b7b717c115e6ae32c0e
Jatin Yadav <heroraoshab2005@gmail.com>
2025-12-12 10:54:16 +0530

    feat: add api-gateway for microservices

--- services/api-gateway/app.js
+++ services/api-gateway/app.js
@@ -1,5 +1,48 @@
-const express = require('express');
+const express = require("express");
+const proxy = require("express-http-proxy");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Service proxies
+app.use(
+  "/users",
+  proxy(process.env.USER_SERVICE_URL, {
+    proxyReqPathResolver: function (req) {
+      return `/users${req.url}`;
+    },
+  })
+);
+
+app.use(
+  "/captains",
+  proxy(process.env.CAPTAINS_SERVICE_URL, {
+    proxyReqPathResolver: function (req) {
+      return `/captains${req.url}`;
+    },
+  })
+);
+
+app.use(
+  "/rides",
+  proxy(process.env.RIDES_SERVICE_URL, {
+    proxyReqPathResolver: function (req) {
+      return `/rides${req.url}`;
+    },
+  })
+);
+
+app.use(
+  "/maps",
+  proxy(process.env.RIDES_SERVICE_URL, {
+    proxyReqPathResolver: function (req) {
+      return `/maps${req.url}`;
+    },
+  })
+);
 
 module.exports = app;
